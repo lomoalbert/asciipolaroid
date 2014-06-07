44209721ea98453fb5be4d6603cd3f6d641662e0
albert <weizhe86@gmail.com>
2014-06-07 14:06:12 +0800

    compatible with python3

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='asciipolaroid',
+    version='0.1.0',
+    packages=[''],
+    url='https://app.weizhe.net/asciipolaroid',
+    license='LGPL2.1',
+    author='albert',
+    author_email='weizhe86@gmail.com',
+    description='convert picture to ascii picture'
+)
